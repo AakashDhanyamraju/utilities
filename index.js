f575ff24bf291fd672e52cabc00b9ba3e0d0bd39
Avaamo <avaamo@Avaamos-MacBook-Pro.local>
2023-08-28 14:30:12 +0530

    added few more usefull functons

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const cheerio = require("cheerio");
 
 function writeJSONFile(filename, data) {
   try {
@@ -29,6 +30,7 @@ function readCSVFile(filename) {
 function readJSONFile(filename) {
   try {
     const data = fs.readFileSync(filename);
+    console.log("🚀 ~ file: readJSONFile ~ DataRead");
     return JSON.parse(data);
   } catch (error) {
     console.error(`Error reading ${filename}: ${error}`);
@@ -53,7 +55,7 @@ function writeCSVfile(path, jsonData) {
   const csvArray = [keys.join(",")];
 
   for (const item of data) {
-    const values = keys.map((key) => item[key]);
+    const values = keys.map((key) => JSON.stringify(item[key]));
     csvArray.push(values.join(","));
   }
 
@@ -70,11 +72,117 @@ function removeDuplicates(arr) {
   return [...new Set(arr)];
 }
 
+function convertCsvToJson(csv_file_path, json_file_path) {
+  const data = [];
+  const csvData = fs.readFileSync(csv_file_path, "utf-8").split("\n");
+  const headers = csvData[0].split(",");
+
+  for (let i = 1; i < csvData.length; i++) {
+    const rowData = csvData[i].split(",");
+    const row = {};
+    for (let j = 0; j < headers.length; j++) {
+      row[headers[j]] = rowData[j];
+    }
+    data.push(row);
+  }
+
+  fs.writeFileSync(json_file_path, JSON.stringify(data, null, 4));
+  console.log(`Data written to ${json_file_path}`);
+}
+
+function convertJsonToCsv(jsonFilePath, csvFilePath) {
+  const jsonData = fs.readFileSync(jsonFilePath, "utf-8");
+  const data = JSON.parse(jsonData);
+
+  if (!Array.isArray(data) || data.length === 0) {
+    throw new Error("Invalid JSON data");
+  }
+
+  const headers = Object.keys(data[0]);
+  const csvRows = [headers.join(",")];
+
+  for (const row of data) {
+    const values = headers.map((header) => row[header]);
+    csvRows.push(values.join(","));
+  }
+
+  fs.writeFileSync(csvFilePath, csvRows.join("\n"));
+}
+
+async function fetchSubUrls(rootUrl) {
+  try {
+    const response = await fetch(rootUrl);
+    const htmlText = await response.text();
+
+    const $ = cheerio.load(htmlText);
+
+    const subUrls = [];
+    $("a").each((index, element) => {
+      const href = $(element).attr("href");
+      if (href && href.startsWith("/") && !href.startsWith("//")) {
+        const subUrl = new URL(href, rootUrl).href;
+        subUrls.push(subUrl);
+      }
+    });
+
+    return subUrls;
+  } catch (error) {
+    console.error("An error occurred:", error);
+    return [];
+  }
+}
+
+function flattenArray(arr) {
+  return arr.reduce((flat, item) => {
+    return flat.concat(Array.isArray(item) ? flattenArray(item) : item);
+  }, []);
+}
+
+function groupBy(arr, property) {
+  return arr.reduce((groups, obj) => {
+    const key = obj[property];
+    if (!groups[key]) groups[key] = [];
+    groups[key].push(obj);
+    return groups;
+  }, {});
+}
+
+function compose(...functions) {
+  return function (input) {
+    return functions.reduceRight((acc, func) => func(acc), input);
+  };
+}
+
+async function fetchSubUrlsEachPage(rootUrl, pageCount) {
+  const allSubUrls = [];
+
+  for (let page = 0; page <= pageCount; page++) {
+    const pageUrl = `${rootUrl}?page=${page}`;
+    const response = await fetch(pageUrl);
+    const htmlText = await response.text();
+
+    const $ = cheerio.load(htmlText);
+    $(".program-cards a").each((index, element) => {
+      const subUrl = $(element).attr("href");
+      allSubUrls.push(subUrl);
+    });
+  }
+
+  return allSubUrls;
+}
+
 module.exports = {
+  convertCsvToJson,
+  convertJsonToCsv,
   writeJSONFile,
-  readCSVFile,
   readJSONFile,
-  calculatePercentage,
   writeCSVfile,
+  readCSVFile,
   removeDuplicates,
+  calculatePercentage,
+  fetchSubUrls,
+  flattenArray,
+  groupBy,
+  compose,
+  fetchSubUrlsEachPage,
 };
